83ace0e023f446cc9960fc6bc36b6d972133c54f
Oliver Gomes <ogomesphoto@gmail.com>
2018-11-14 20:56:24 -0500

    footer added

--- src/Hoc/Layout.js
+++ src/Hoc/Layout.js
@@ -1,11 +1,14 @@
 import React from "react";
+
 import Header from "../Components/Header-Footer/Header";
+import Footer from "../Components/Header-Footer/Footer";
 
 const Layout = props => {
   return (
     <div>
       <Header />
       {props.children}
+      <Footer />
     </div>
   );
 };
